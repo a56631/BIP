5fbd33f4a8ed175dffd36f7b2107e578b54a4c01
João Tomás de Liz Hipólito Duarte <a56631@alunos.ipb.pt>
2025-06-09 20:27:26 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import logo from './assets/logozan.png';
 import logo2 from './assets/logo2.png';
-import logo3 from './assets/logoyou.png';
-import logo4 from './assets/logofac.png'; 
-import logo5 from './assets/logoins.png';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import translations from './translations';
 import Home from './screens/home.js';
@@ -51,7 +48,7 @@ function App() {
           <Link to="/map" className="TopBarBox">{translations.topBar[language].map}</Link>
         </div>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/BIP" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home language={language} />} />
           <Route path="/landmarks" element={<Landmarks language={language} />} />
           <Route path="/gastronomy" element={<Gastronomy language={language} />} />
